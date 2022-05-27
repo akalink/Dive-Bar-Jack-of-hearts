d2fdde36818cbd0157320706eb3b1275966ae4eb
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-05-26 20:36:00 -0700

    added look at changes for npc

--- Assets/my-scripts/lookattoggle.cs
+++ Assets/my-scripts/lookattoggle.cs
@@ -6,22 +6,32 @@ using UnityEngine;
 using UnityEngine.Animations;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 namespace akaUdon
 {
-
-    public class lookattoggle : UdonSharpBehaviour
+    public class LookAtToggle : UdonSharpBehaviour
     {
-        public Animator _animator;
-        public string name;
-        public string talk;
-        public TextAsset file;
+        #region Serialized
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string lookAtName;
+        [SerializeField] private string talk;
+        [SerializeField] private string responsesShow;
+        [SerializeField] private  TextAsset file;
+        [SerializeField] private TextAsset answers;
+        [SerializeField] private TextMeshProUGUI textBox;
+        #endregion
+
+        #region not serialized
         private string[] lines;
-        public TextMeshProUGUI textBox;
-        private int stringIndex = 0;
+        private int stringIndex;
         private string tempString;
         private int playerTalking = -1;
-
+        private int currentLine;
+        
+        #endregion
+        
+        
         void Start()
         {
             lines = file.text.Split('\n');
@@ -29,7 +39,7 @@ namespace akaUdon
 
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
         {
-            if (playerTalking == -1)
+            if (playerTalking == -1) //if the conversation is not being occupied
             {
                 playerTalking = player.playerId;
                 
@@ -37,7 +47,11 @@ namespace akaUdon
                 {
                     lines[i] = lines[i].Replace("*", player.displayName);
                 }
-                _animator.SetBool(name, true);
+
+                if (player.playerId == playerTalking) //makes the look at constraint only work for the person in the conversation
+                {
+                    _animator.SetBool(lookAtName, true);
+                }
                 _animator.SetTrigger(talk);
                 InitializeString(0);
             }
@@ -59,6 +73,10 @@ namespace akaUdon
                 stringIndex++;
                 SendCustomEventDelayedFrames(nameof(StringLoad), 2);
             }
+            else
+            {
+                _animator.SetBool(responsesShow, true);
+            }
         }
 
         public override void OnPlayerTriggerExit(VRCPlayerApi player)
@@ -66,9 +84,22 @@ namespace akaUdon
             if (player.playerId == playerTalking)
             {
                 playerTalking = -1;
-                _animator.SetBool(name, false);
+                _animator.SetBool(lookAtName, false);
                 lines = file.text.Split('\n');
             }
+        }
+
+        public void FirstAnswer()
+        {
+            if (Networking.LocalPlayer.playerId == playerTalking)
+            {
+                SendCustomNetworkEvent(NetworkEventTarget.All,nameof(FirstAnswerNetwork));
+            }
+        }
+
+        private void FirstAnswerNetwork()
+        {
+            Debug.Log("this is an answer");   
         }
     }
 }
